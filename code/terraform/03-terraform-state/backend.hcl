b5de8e91bfeb8012140e076a6f5170ea1fd08bc8
YasudaKaito <ykit.engineer@gmail.com>
2025-02-16 12:26:39 +0900

    [add] 3.3 backend.hclにバックエンド情報を保存

---
+++ code/terraform/03-terraform-state/backend.hcl
@@ -0,0 +1,4 @@
+bucket         = "terraform-up-and-running-state-xxxx"
+region         = "us-east-2"
+dynamodb_table = "terraform-up-and-running-locks"
+encrypt        = true
--- code/terraform/03-terraform-state/main.tf
+++ code/terraform/03-terraform-state/main.tf
@@ -1,10 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-up-and-running-state-xxxx"
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt        = true
+    key = "example/terraform.tfstate"
   }
 }
 
