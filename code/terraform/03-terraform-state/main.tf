07378b9977b351d2df992514d9a388c4d6233f25
YasudaKaito <ykit.engineer@gmail.com>
2025-02-16 12:16:38 +0900

    [add] 3.2 S3にtfstateを保存、DynamoDBにlockを保存

---
+++ code/terraform/03-terraform-state/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-up-and-running-state-xxxx"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-up-and-running-state-xxxx"
+
+  # 誤ってバケット削除するのを防止
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# ステートファイルの履歴が見られるように、バージョニングを有効化
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# 明示的にバケットに対する全パブリックアクセスをブロック
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DDB table"
+}
