6d217b3bbb340dae227abad9013e8ddef684358c
YasudaKaito <ykit.engineer@gmail.com>
2025-02-19 17:53:16 +0900

    [fix] prod の backend の key を修正

--- code/terraform/04-terraform-module/module-example/prod/data-stores/mysql/main.tf
+++ code/terraform/04-terraform-module/module-example/prod/data-stores/mysql/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-up-and-running-state-xxxx"
-    key            = "stage/data-stores/mysql/terraform.tfstate"
+    key            = "prod/data-stores/mysql/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "terraform-up-and-running-locks"
     encrypt        = true
