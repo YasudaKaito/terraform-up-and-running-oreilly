ced2619671660ccead7b377ee643e353a569aef4
YasudaKaito <ykit.engineer@gmail.com>
2025-02-22 17:54:24 +0900

    [add] 5.1.2 for_eachの例

--- code/terraform/05-tips-and-tricks/live/global/iam/main.tf
+++ code/terraform/05-tips-and-tricks/live/global/iam/main.tf
@@ -2,13 +2,28 @@ provider "aws" {
   region = "us-east-2"
 }
 
+# countの例
 # resource "aws_iam_user" "example" {
 #   count = length(var.user_names)
 #   name  = var.user_names[count.index]
 # }
 
+# countを使用しモジュールに引数user_nameを渡す
+# module "users" {
+#   source    = "../../../modules/landing-zone/iam-user"
+#   count     = length(var.user_names)
+#   user_name = var.user_names[count.index]
+# }
+
+# for_eachの例
+# resource "aws_iam_user" "example" {
+#   for_each = toset(var.user_names)
+#   name     = each.value
+# }
+
+# for_each + module で引数 user_name を渡す
 module "users" {
   source    = "../../../modules/landing-zone/iam-user"
-  count     = length(var.user_names)
-  user_name = var.user_names[count.index]
+  for_each  = toset(var.user_names)
+  user_name = each.value
 }
--- code/terraform/05-tips-and-tricks/live/global/iam/outputs.tf
+++ code/terraform/05-tips-and-tricks/live/global/iam/outputs.tf
@@ -8,7 +8,21 @@
 #   description = "The ARNs for all users"
 # }
 
+# output "user_arns" {
+#   value       = module.users[*].user_arn
+#   description = "The ARNs of the created IAM users"
+# }
+
+# output "all_users" {
+#   value = aws_iam_user.example
+# }
+
+# output "all_arns" {
+#   value = values(aws_iam_user.example)[*].arn
+# }
+
+# moduleのoutputを参照
 output "user_arns" {
-  value       = module.users[*].user_arn
+  value       = values(module.users)[*].user_arn
   description = "The ARNs of the created IAM users"
 }
